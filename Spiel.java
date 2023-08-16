99724b0930e65da5a75d103cb944fc0a1a3c0676
Flower2121 <142184449+Flower2121@users.noreply.github.com>
2023-08-16 02:51:08 +0200

    Update Spiel.java

--- Spiel.java
+++ Spiel.java
@@ -1,27 +1,57 @@
 package risiko;
 
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.io.*;
 
 public class Spiel {
 	public boolean gewinner;
 	public int phase;
 	public int anderreihe;
+	public int players;
 	public Phasen runde;
 	ArrayList<Spieler> spielende;
+	Color colors[] = {Color.red,Color.green,Color.blue,Color.yellow,Color.ORANGE,Color.MAGENTA};
+	Welt erde = new Welt();
 	
-	Spiel (){
+	Spiel (int play,int bots,Welt welt){
+		spielende = new ArrayList<Spieler>();
 		Spieler player;
-		menue menü = new menue();
 		anderreihe =0;
 		gewinner =false;
-		for(int i=0; i<menü.getplayercount();i++) {
-			if (i<menü.getplayercount()-menü.getbots()) {
+		players=play;
+		erde = welt;
+		for(int i=0; i<players;i++) {
+			if (i<players-bots) {
 				player = new Spieler (true);
 			}else {
 				player = new Spieler (false);
 			}
 			spielende.add(player);
+			spielende.get(i).color = colors[i];
 		}
+	}
+	void landverteilung(Welt erde) {
+		int dran=0;
+		int halte;
+		Random rand = new Random();
+		ArrayList<Land> hold = new ArrayList<Land>();
+		for (int i=0; i < erde.laender.length;i++) {
+			hold.add(erde.laender[i]);
+		}
+		while (hold.size()>0) {
+			halte = rand.nextInt(hold.size());
+			spielende.get(dran).erobert(hold.get(halte));
+			spielende.get(dran).besetzt.get(spielende.get(dran).besetzt.size()-1).setbesetzer(spielende.get(dran), 1);
+			hold.remove(halte);
+			dran+=1;
+			if (dran>=spielende.size()) dran=0;
+		}
+		
 	}
 	
 	void Runde(){
@@ -43,5 +73,21 @@ public class Spiel {
 		}
 		
 	}
+	void truppenverteilung (Welt erde){
+		int verteilbar;
+		verteilbar = 50 - 5*spielende.size();
+		for (int i =0;i<spielende.size();i++) {
+			spielende.get(i).truppen = verteilbar;
+		}
+		actionlistener lis = new actionlistener(this);
+		for(int i = 0; i <42;i++) {
+			erde.gui1.list[i].addActionListener(lis);
+  
+		}
+
+			verteilbar -=1;
+			}
+
+		 
+	}
 	
-}
